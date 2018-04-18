127ea9d534d7be125885882247af37ff0ec2b07a
mariesonko62@gmail.com <mariesonko62@gmail.com>
2018-04-18 15:33:12 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,7 @@
 def square_array(array)
   array.each do |numbers|
     puts numbers
-    new_numbers = Math.sqrt numbers
-    square_array(new_numbers)
   end
+  new_numbers = Math.sqrt numbers
+  square_array(new_numbers)
 end
